ad6949eed1fb3177deb5235f313cdf56f1e0f631
nicholasstano <nicholasstano@gmail.com>
2019-06-25 17:27:33 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+require 'byebug'
+
+class School
+
+attr_accessor :school_name, :roster, :student_name, :grade_level, :new_roster
+
+	def initialize(school_name)
+		@school_name = school_name
+		@roster = {}
+	end
+
+	def add_student(student_name, grade_level)
+		@roster[grade_level] ||= []
+		@roster[grade_level] << student_name
+	end
+
+	def grade(grade_level)
+		@roster[grade_level].collect do |students|
+			students
+		end
+	end
+
+	def sort
+		@roster.collect do |grade_level, student_name|
+			@roster[grade_level] = student_name.sort
+		end
+		@roster
+	end
+end

a063d04701fca1ee71c9efaee4ee6f00bac343d9
adriennem76 <adrienne.c.miranda@gmail.com>
2019-06-25 16:34:29 -0400

    school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+
+class School
+
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        @roster.collect do |grade, students|
+            @roster[grade] = students.sort
+            # binding.pry
+        end
+        @roster
+    end
+            
+end
